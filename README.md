279e3469d8ca59b37dacf89417ec1c29214b65fa
SergejBusch <herimvsem@gmail.com>
2021-06-28 16:35:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+#cinema_booking
+
---
+++ src/main/test/java/ru/job4j/AppTest.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+}
